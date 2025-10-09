19a10108deb35768b345eed3284759566ed62708
Mateusz Nurczyński <mnurczynski4@gmail.com>
2025-10-09 21:35:41 +0200

    base-core: Using  task to initialize program memory
    Signed-off-by: Mateusz Nurczyński <mnurczynski4@gmail.com>

--- base-core/design/program_memory/basic_async_rom.v
+++ base-core/design/program_memory/basic_async_rom.v
@@ -1,4 +1,7 @@
 module program_memory
+  #(
+    parameter PROGRAM_HEX_FILE
+  )
   (
     output [23:0] output_bus,
     input [15:0] address,
@@ -6,17 +9,14 @@ module program_memory
     output instruction_ready
   );
 
-  reg [23:0] data[21844:0];
+  reg [23:0] data[0:21844];
 
   initial begin
-    data[0] = 24'b000111110000000101010101;
-    data[1] = 24'b110011111111111100000001;
-    data[2] = 24'b111011111000000011000000;
-    data[3] = 24'b001111110000011010000000;
-    data[4] = 24'b001011110000001000000110;
-    data[5] = 24'b000111110001100000000000;
-    data[6] = 24'b011011110000000000000000;
-    data[7] = 24'b010011110001000000001000;
+    if(PROGRAM_HEX_FILE=="") begin
+      $display("WARNING: No code file provided - initializing program memory to random values");
+    end else begin
+      $readmemh(PROGRAM_HEX_FILE, data, 0);
+    end
   end
 
   wire[15:0] act_address = address/3;
