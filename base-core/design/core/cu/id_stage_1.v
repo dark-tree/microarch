8efdbcee64460079dc3e954ef04a0614069ad9bc
Mateusz Nurczyński <mnurczynski4@gmail.com>
2025-12-06 10:58:20 +0100

    base-core: core: cu: id_stage_1: Fixed register dependency manaement circuit for STM and LDM instructions
    Signed-off-by: Mateusz Nurczyński <mnurczynski4@gmail.com>

--- base-core/design/core/cu/id_stage_1.v
+++ base-core/design/core/cu/id_stage_1.v
@@ -68,8 +68,8 @@ module id_stage_1
   wire alu_bus_a_read = (instruction[23] == 1'b1) & (instruction[22:20] != 3'b100);
   wire alu_bus_b_read = (instruction[23] == 1'b1) & (instruction[22:20] != 3'b011);
 
-  wire bus_a_read = alu_bus_a_read | (instruction[23:20] == 4'b0100) | (instruction[23:21] == 3'b001) | (instruction[23:20] == 4'b0111);
-  wire bus_b_read = alu_bus_b_read | (instruction[23:20] == 4'b0100) | (instruction[23:20] == 4'b0011);
+  wire bus_a_read = alu_bus_a_read | (instruction[23:20] == 4'b0100) | (instruction[23:20] == 4'b0011) | (instruction[23:20] == 4'b0111);
+  wire bus_b_read = alu_bus_b_read | (instruction[23:20] == 4'b0100) | (instruction[23:21] == 3'b001);
 
   wire[7:0] registers_read_a = bus_a_read ? instruction[15:8] : 8'b00000000;
   wire[7:0] registers_read_b = bus_b_read ? instruction[7:0] : 8'b00000000;
